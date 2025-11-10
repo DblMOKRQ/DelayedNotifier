a4750afb3148174d8d7e89558756a0acc69d97c4
Karimov Ravil <161382526+DblMOKRQ@users.noreply.github.com>
2025-11-10 13:49:04 +0300

    Refactor LoggingMiddleware to use ginext package

--- internal/router/middleware/middleware.go
+++ internal/router/middleware/middleware.go
@@ -1,12 +1,12 @@
 package middleware
 
 import (
-	"github.com/gin-gonic/gin"
+	"github.com/wb-go/wbf/ginext"
 	"go.uber.org/zap"
 )
 
-func LoggingMiddleware(log *zap.Logger) gin.HandlerFunc {
-	return func(c *gin.Context) {
+func LoggingMiddleware(log *zap.Logger) ginext.HandlerFunc {
+	return func(c *ginext.Context) {
 		requestLog := log.With(
 			zap.String("method", c.Request.Method),
 			zap.String("path", c.Request.URL.Path),
